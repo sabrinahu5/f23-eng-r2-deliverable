07b1d3cd23ae7f36b5688646b38ee4d10063d756
SABRINA HU <sabrinahu@college.harvard.edu>
2023-09-17 22:27:01 -0400

    add detailed view functionality

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
+import { useState } from "react";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard(species: Species) {
+  const [open, setOpen] = useState<boolean>(false); // maintain a state to control detailed view
+
+  const toggleView = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -14,8 +31,25 @@ export default function SpeciesCard(species: Species) {
       <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
       <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
-      {/* Replace with detailed view */}
-      <Button className="mt-3 w-full">Learn More</Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button
+            className="mt-3 w-full"
+            onClick={() => setOpen(true)}>
+          >
+            Learn More
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>{species.common_name}</DialogTitle>
+            <DialogDescription>Scientific Name: {species.scientific_name}</DialogDescription>
+            <DialogDescription>Total Population: {species.total_population}</DialogDescription>
+            <DialogDescription>Kingdom: {species.kingdom}</DialogDescription>
+            <DialogDescription>Description: {species.description}</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
